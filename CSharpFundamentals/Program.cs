c124b8e570b4841eb1958282006be6ad0a245750
Bishnu Rawal <rawal.bishnu@gmail.com>
2022-07-28 10:09:12 +0545

    Add class work solution 2

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -3,3 +3,12 @@
 var firstName = "Ram";
 Console.WriteLine($"Hello, {firstName}");
 
+// Class work 1
+
+
+//class work 2
+Console.Write("Enter length in meter: ");
+var length = Console.ReadLine();
+var lengthNumber = double.Parse(length);
+var feets = lengthNumber * 3.2808;
+Console.WriteLine($"{lengthNumber} metres = {feets} feet");
--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # NET-Classes-Bishnu
-
+## Classwork
 1. Program to capitalize entered text.
 2. Program to convert meter to feet. Output: n metre = m feet
+
+## Assignments
+
+
+## Projects
