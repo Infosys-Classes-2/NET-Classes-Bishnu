6f4db98ae85ab83a5e6f63d14ec2921ff091f6e8
Bishnu Rawal <rawal.bishnu@gmail.com>
2022-08-04 10:50:45 +0545

    Discussion on constructors

--- CSharpFundamentals/Lessions/0-EntryPoint.cs
+++ CSharpFundamentals/Lessions/0-EntryPoint.cs
@@ -20,15 +20,18 @@ class TestClasses
         // Assignment2 assignment2 = new();
         // assignment2.PrintPattern();
 
-        MethodLearning ml = new();
-        ml.PrintNepal();
-        ml.PrintNepal(45);
+        // MethodLearning ml = new();
+        // ml.PrintNepal();
+        // ml.PrintNepal(45);
         // var s = ml.Add(3453, 35335);//squiggle
         // var x = ml.GetInitials("Milan sfafsdf Dhakal");
         // Console.WriteLine(x);
 
-        short[] ages = {45, 56, 23, 34, 12, -23, -12, 0};
-        var (min, max) = ml.GetMinMax(ages);
-        Console.WriteLine($"Minimum: {min}, Maximum: {max}");
+        // short[] ages = {45, 56, 23, 34, 12, -23, -12, 0};
+        // var (min, max) = ml.GetMinMax(ages);
+        // Console.WriteLine($"Minimum: {min}, Maximum: {max}");
+
+        Shape2D shape = new("Rectangle");
+        shape.PrintDetail();
     }
 }
