68480fd07872908d134ab5d2adc3684f6b896a70
Bishnu Rawal <rawal.bishnu@gmail.com>
2022-08-22 10:55:54 +0545

    Add web mvc template

---
+++ HelloWeb/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWeb.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public string Test()
+    {
+        return "Hello there!!!";
+    }
+}
