4b93ee599cea6e4f3d9fdb20c03973648ea5be44
Bishnu Rawal <rawal.bishnu@gmail.com>
2022-08-23 11:07:13 +0545

    Add test controller

---
+++ HelloWeb/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWeb.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Hello()
+        {
+            return View();
+        }
+
+        public IActionResult Print(int limit)
+        {
+            return View(limit);
+        }
+    }
+}
