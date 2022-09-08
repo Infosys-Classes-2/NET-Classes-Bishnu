fe51106fd9b4e9ebf57ad1b240e122fd931b1282
Bishnu Rawal <rawal.bishnu@gmail.com>
2022-09-08 10:53:24 +0545

    Add auth

--- HRM/HRM.Web/Program.cs
+++ HRM/HRM.Web/Program.cs
@@ -1,10 +1,15 @@
 using HRM.Web.Data;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<EmployeeContext>();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => 
+    options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<EmployeeContext>();
 
 var app = builder.Build();
 
@@ -13,8 +18,12 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 
 app.Run();
