4b74c86e26beb4cd89c09a1470a2b15b5bc36b4c
Bishnu Rawal <rawal.bishnu@gmail.com>
2022-09-18 10:47:46 +0545

    Add repositories

--- HRM/HRM.Web/Program.cs
+++ HRM/HRM.Web/Program.cs
@@ -1,3 +1,4 @@
+using HRM.Infrastructure.Repositories;
 using HRM.Web.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -9,8 +10,13 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<EmployeeContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("hrmConnection")));
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => 
+builder.Services.AddScoped<EmployeeRepository>();
+builder.Services.AddScoped<DepartmentRepository>();
+builder.Services.AddScoped<DesignationRepository>();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
     options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<EmployeeContext>();
 
 builder.Services.AddRazorPages();
